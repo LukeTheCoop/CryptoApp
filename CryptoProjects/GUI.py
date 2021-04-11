37ee20c190a442439bd7a581e49303377b82b716
LukeTheCoop <54200205+LukeTheCoop@users.noreply.github.com>
2021-04-11 16:23:53 -0700

    Update GUI.py

--- CryptoProjects/GUI.py
+++ CryptoProjects/GUI.py
@@ -1,4 +1,9 @@
-import pygame, os, tracker, time
+import pygame
+import os
+import time 
+from tracker import *
+import string
+
 pygame.font.init()
 
 WIDTH, HEIGHT = 500, 900
@@ -15,6 +20,8 @@ INPUT_BORDER = pygame.Rect(WIDTH//2 - 120, 50, 240, 50)
 ENTER_BORDER = pygame.Rect(WIDTH//2 - 60, INPUT_BORDER.y + 100, 120, 40)
 CLEAR_BORDER = pygame.Rect(WIDTH//2 - 41, ENTER_BORDER.y + 50, 82, 25)
 
+TRACK_INPUT_BORDER = pygame.Rect(WIDTH//2 - 120, HEIGHT//2 - 25 - 50, 240, 50)
+TRACK_ENTER = pygame.Rect(WIDTH//2 - 60, TRACK_INPUT_BORDER.y + 80, 120, 40)
 
 BASE_FONT = pygame.font.SysFont('timesnewroman', 40)
 BASE_BOLD = pygame.font.SysFont('bold_timesnewroman', 80)
@@ -26,10 +33,14 @@ SMALL_FONT = pygame.font.SysFont('timesnewroman', 14)
 MAIN_TRACK_FONT = pygame.font.SysFont('timesnewroman', 25)
 GOAL_TRACK_FONT = pygame.font.SysFont('timesnewroman', 16)
 
+#X marks
+X_ONE, X_TWO, X_THREE = pygame.Rect(395, 310, 25, 25), pygame.Rect(395, 430, 25, 25), pygame.Rect(395, 550, 25, 25) 
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED =  (255, 0, 0)
 GRAY = (134, 134, 134)
+GREEN = (0, 255, 0)
 
 FPS = 60
 
@@ -43,19 +54,24 @@ def header():
 def get_tracked():
 	track_height = 310
 
-	for i in range(3):
+	for i in get_tracked_list():
 		TRACK_RECT = pygame.Rect(WIDTH//2 - 350//2, track_height, 350, 80)
 
-
+        
 		#Draws
+
 		pygame.draw.rect(WIN, WHITE, TRACK_RECT)
 		pygame.draw.rect(WIN, BLACK, TRACK_RECT, 5)
 
 		WIN.blit(XImage, (TRACK_RECT.x, TRACK_RECT.y))
 
 		#Text
-		main_text = MAIN_TRACK_FONT.render('Bitcoin: $39,567.22', True, BLACK)
-		goal_text = GOAL_TRACK_FONT.render('Alert: $40,000', True, BLACK)
+		crypto, goal = i.split('-')[0], i.split('-')[1]
+
+		price = get_currency(crypto)
+
+		main_text = MAIN_TRACK_FONT.render(crypto.title() + ': ${:,}'.format(price), True, BLACK)
+		goal_text = GOAL_TRACK_FONT.render('Alert: ${:,}'.format(float(goal)), True, BLACK)
 
 		
 		WIN.blit(main_text, (TRACK_RECT.x + 8, TRACK_RECT.y + 8))
@@ -64,19 +80,20 @@ def get_tracked():
 		WIN.blit(goal_text, (x - goal_text.get_width() - 5, y - goal_text.get_height() - 5))
 
 		track_height += 120
-
-def checkHover(enter_hover, reset_hover):
+	
+def checkHover():
 	enter_text = BASE_FONT.render('Enter', True, BLACK)
 	small_text = SMALL_FONT.render('Clear tracked', True, BLACK)
 
-	if enter_hover:
+
+	if ENTER_BORDER.collidepoint(pygame.mouse.get_pos()):
 		pygame.draw.rect(WIN, GRAY, ENTER_BORDER)
 		WIN.blit(enter_text, (ENTER_BORDER.x + 15, ENTER_BORDER.y - 3))
 	else:
 		pygame.draw.rect(WIN, BLACK, ENTER_BORDER, 2)
 		WIN.blit(enter_text, (ENTER_BORDER.x + 15, ENTER_BORDER.y - 3))
 
-	if reset_hover:
+	if CLEAR_BORDER.collidepoint(pygame.mouse.get_pos()):
 		pygame.draw.rect(WIN, GRAY, CLEAR_BORDER)
 		WIN.blit(small_text, (CLEAR_BORDER.x + 3, CLEAR_BORDER.y + 5))
 	else:
@@ -84,14 +101,15 @@ def checkHover(enter_hover, reset_hover):
 		WIN.blit(small_text, (CLEAR_BORDER.x + 3, CLEAR_BORDER.y + 5))
 
 
-def draw_window(user_text, status, enter_hover, reset_hover):
+
+def draw_window(user_text, status):
 	search_text = BASE_FONT.render(user_text, True, BLACK)
 	status_text = STATUS_FONT.render(status, True, RED)
 	track_title_text = BASE_BOLD.render('Tracked:',  True, WHITE)
 
 	header()
 
-	checkHover(enter_hover, reset_hover)
+	checkHover()
 
 	pygame.draw.rect(WIN, BLACK, INPUT_BORDER, 4)
 	WIN.blit(search_text, (INPUT_BORDER.x + 5, INPUT_BORDER.y))
@@ -107,12 +125,19 @@ def draw_window(user_text, status, enter_hover, reset_hover):
 
 
 
-def draw_crypto(cur):
+def draw_crypto(cur, user_text):
 	#Call functionprice = tracker.get_currency(cur)
-	price = tracker.get_currency(cur)
+	price = get_currency(cur)
 
 	title_text = BASE_BOLD.render(cur.title(), True, BLACK)
 	price_string = 'Crypto Price: ${:,}'.format(price)
+
+	track_text = BASE_FONT.render('Enter alert price', True, BLACK)
+	sub_track_text = STATUS_FONT.render('(Leave blank if none)', True, BLACK)
+
+	track_add_text = BASE_FONT.render('Enter', True, BLACK)
+	added_text = BASE_FONT.render(user_text, True, BLACK)
+	#price_string =  str(price)
 	crypto_text = BASE_FONT.render(price_string, True, BLACK)
 
 	#header
@@ -123,20 +148,35 @@ def draw_crypto(cur):
 
 	WIN.blit(crypto_text, (WIDTH//2 - crypto_text.get_width()//2, title_text.get_height() + 100))
 
+	#Track currency
+	WIN.blit(track_text, (WIDTH//2 - track_text.get_width()//2, TRACK_INPUT_BORDER.y - 100))
+	WIN.blit(sub_track_text, (WIDTH//2 - sub_track_text.get_width()//2, TRACK_INPUT_BORDER.y - 50))
+
+	pygame.draw.rect(WIN, WHITE, TRACK_INPUT_BORDER)
+	pygame.draw.rect(WIN, BLACK, TRACK_INPUT_BORDER, 4)
+
+	WIN.blit(added_text, (TRACK_INPUT_BORDER.x + 5, TRACK_INPUT_BORDER.y))
+
+	pygame.draw.rect(WIN, BLACK, TRACK_ENTER, 2)
+	WIN.blit(track_add_text, (TRACK_ENTER.x + 15, TRACK_ENTER.y - 2))
+
+
 	pygame.display.update()
 
 
 def crypto_window(text):
 	clock = pygame.time.Clock()
 
-	print('run')
 	error = 'Currency couldn\'t be found!'
-	if tracker.check_currency(text):
+	if check_currency(text):
 		run = True
 	else:
 		main(error)
 		run = False
-	
+
+	user_text = ''
+	type_active = False
+
 	while run:
 		clock.tick(FPS)
 		for event in pygame.event.get():
@@ -146,10 +186,60 @@ def crypto_window(text):
 				break
 
 			if event.type == pygame.KEYDOWN:
+
 				if event.key == pygame.K_ESCAPE:
 					main('')
 					run = False
-		draw_crypto(text)
+
+				if event.key == pygame.K_BACKSPACE:
+					user_text = user_text[:-1]
+				elif type_active:
+					if event.key != pygame.K_RETURN:
+						user_text += event.unicode
+				
+				if event.key == pygame.K_RETURN:
+					if len(get_tracked_list()) <= 3:
+						type_active = False
+						finished_text = user_text.lower()
+
+						if finished_text != '':
+							try:
+								float(user_text)
+								add_track(text, user_text)
+								main('Alert added!')
+								run = False
+								break
+							except:
+								main('Please make sure to enter a number!')
+						else:
+							pass
+					else:
+						main('You have reached the max amount of tracked currency\'s (3/3)')
+						run = False
+						break
+
+
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if TRACK_INPUT_BORDER.collidepoint(event.pos) and type_active == False:
+					type_active = True
+				else:
+					type_active = False
+
+				if TRACK_ENTER.collidepoint(event.pos):
+					type_active = False
+					finished_text = user_text.lower()
+					if finished_text != '':
+						try:
+							float(user_text)
+							add_track(text, user_text)
+							main('Alert added!')
+							run = False
+							break
+						except:
+							main('Please make sure to enter a number!')
+					else:
+						pass
+		draw_crypto(text, user_text)
 
 def main(status):
 	clock = pygame.time.Clock()
@@ -188,9 +278,28 @@ def main(status):
 
 					run = False
 
+				if CLEAR_BORDER.collidepoint(event.pos):
+					clear_tracked_list()
+					run = False
+					main('Cleared')
+					break
+
+				if X_ONE.collidepoint(event.pos):
+					if len(get_tracked_list()) >= 1:
+						remove_track(0)
+				elif X_TWO.collidepoint(event.pos):
+					if len(get_tracked_list()) >= 2:
+						remove_track(1)
+				elif X_THREE.collidepoint(event.pos):
+					if len(get_tracked_list()) >= 3:
+						remove_track(2)
+
+
+
 
 
 			if event.type == pygame.KEYDOWN:
+
 				if event.key == pygame.K_BACKSPACE:
 					user_text = user_text[:-1]
 				elif type_active:
@@ -210,18 +319,10 @@ def main(status):
 		else:
 			type_active = True
 
-		if ENTER_BORDER.collidepoint(pygame.mouse.get_pos()):
-			enter_hover = True
-		else:
-			enter_hover = False
 
-		if CLEAR_BORDER.collidepoint(pygame.mouse.get_pos()):
-			reset_hover = True
-		else:
-			reset_hover = False
-
-		draw_window(user_text, status, enter_hover, reset_hover)
+		draw_window(user_text, status)
 
 
 if __name__ == '__main__':
 	main('')
+	#crypto_window('bitcoin')
